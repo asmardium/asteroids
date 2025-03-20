684d00d82b752300139c8c958e8b51cf59d5139e
asmardium <14826925+asmardium@users.noreply.github.com>
2025-03-19 17:40:20 -0700

    updated asteroids to draw player triangle

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
 	print("Starting Asteroids!")
@@ -11,7 +12,14 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #This is a Surface class object in pygame
+	clock = pygame.time.Clock()
+	dt = 0
 	whileLoop = True
+
+	player_x = SCREEN_WIDTH / 2
+	player_y = SCREEN_HEIGHT / 2
+	thePlayer = Player(player_x, player_y)
+
 	while(whileLoop):
 		#enable the "x" close window button in upper right corner
 		for event in pygame.event.get():
@@ -22,11 +30,19 @@ def main():
 		color_black = pygame.Color(0,0,0)
 		color_white = pygame.Color(255,255,255)
 
-		#draw graphics on screen
+		#draw background graphics on screen
 		screen.fill(color=color_black)
+
+		#draw player on screen
+		thePlayer.draw(screen)
 
 		#refresh display
 		pygame.display.flip()
+
+		#delay starting the next game loop until 60 milliseconds have passed; 
+		#we also retrieve amount of time passed since last tick (delta time) 
+		#also convert dt from milliseconds to seconds
+		dt = clock.tick(60) / 1000 
 
 
 if __name__ == "__main__":
